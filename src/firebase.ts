849cd46fd49d8d790619a1912852d8e0b718ef52
Arwer234 <filipowski.kuba@gmail.com>
2024-01-28 10:39:13 +0100

    chore: remove comments

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyCTGtfPKcwu6Cd48QQtKdCfpP6PUJQKRNo',
   authDomain: 'melody-craft.firebaseapp.com',
@@ -19,4 +13,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
